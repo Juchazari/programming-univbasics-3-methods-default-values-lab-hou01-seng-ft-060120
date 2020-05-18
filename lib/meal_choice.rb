8499e9888d62aa7e378f927a1d5993e65ab1b7b2
Julio Chazari <julio.chazari@gmail.com>
2020-05-18 06:04:04 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,5 +3,5 @@ def meal_choice(veg1, veg2, protein = "tofu")
   return "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-meal_choice('carrots', 'string beans', '')
+meal_choice('carrots', 'string beans', 'lentils')
 # For output purposes, use "puts" instead of "print" or "p"
